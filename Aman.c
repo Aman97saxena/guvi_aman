66c6790cc0abce6be07b1d26c3545d564c09d42d
aman_saxena <aman.saxena_cs15@gla.ac.in>
2018-01-21 22:12:08 +0530

    first prog

---
+++ Aman.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter a number");
+scanf("%d",&n);
+if(n>0)
+{ 
+printf("Number is positive");
+}
+else if
+{
+printf("Number is negative");
+}
+else
+{
+print("Number is zero");
+}
+}
