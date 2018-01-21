b7ede0117e84c2b5931a05f5aff4a1be811feadb
aman_saxena <aman.saxena_cs15@gla.ac.in>
2018-01-21 22:42:02 +0530

    second prog

---
+++ 2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter a number to be checked");
+scanf("%d",&a);
+if(a%2==0){
+printf("Number is even");
+}
+else
+{
+printf("Number is odd");
+}
+}
